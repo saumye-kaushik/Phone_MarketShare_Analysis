c1b8ea63e1638f86c7b570e689d3db19828a0f21
Saumye Kaushik <saumye.k194@gmail.com>
2019-04-28 16:22:27 -0500

    Added PERT distribution for new invention success, added yearly score to pandas dataframe

--- Phone_Marketshare.py
+++ Phone_Marketshare.py
@@ -23,6 +23,40 @@ import random
 import matplotlib as plt
 import seaborn as sb
 
+desired_width = 700
+pd.set_option('display.width', desired_width)
+pd.set_option('display.max_columns', 10)
+
+
+def mod_pert_random(low, likely, high, confidence=4, samples=1):
+    """Produce random numbers according to the 'Modified PERT'
+    distribution.
+    Picked up from the class 12 lecture notes.
+
+    :param low: The lowest value expected as possible.
+    :param likely: The 'most likely' value, statistically, the mode.
+    :param high: The highest value expected as possible.
+    :param confidence: This is typically called 'lambda' in literature
+                        about the Modified PERT distribution. The value
+                        4 here matches the standard PERT curve. Higher
+                        values indicate higher confidence in the mode.
+                        Currently allows values 1-18
+    :param samples: The highest value expected as possible.
+    Formulas from "Modified Pert Simulation" by Paulo Buchsbaum.
+    """
+    # Check minimum & maximum confidence levels to allow:
+    if confidence < 1 or confidence > 18:
+        raise ValueError('confidence value must be in range 1-18.')
+
+    mean = (low + confidence * likely + high) / (confidence + 2)
+
+    a = (mean - low) / (high - low) * (confidence + 2)
+    b = ((confidence + 1) * high - low - confidence * likely) / (high - low)
+
+    beta = np.random.beta(a, b, samples)
+    beta = beta * (high - low) + low
+    return beta
+
 
 # Function for reading input
 def input_file(file) -> pd.DataFrame:
@@ -110,13 +144,15 @@ def revenue_weight(input_df: pd.DataFrame, num_years: int = 5):
     revenue_score_avg = calculate_average(revenue_score_list)
     return revenue_score_avg
 
+
 # Function for calculating historical weightage
 def calculate_previous_data_weight(score_list: list) -> int:
     weighted_score = score_list[0]*40 + score_list[1]*15 + score_list[2]*15 + score_list[3]*30
     return weighted_score
 
-# Function for simulating service partnership, if service partnership increases it shall give the company a postive boost, if the service
-# partnership remains the same or decreases it will affect the company negatively.
+
+# Function for simulating service partnership, if service partnership increases it shall give the company a postive
+# boost, if the service partnership remains the same or decreases it will affect the company negatively.
 def service_partnership():
     sp = [True]
     i = 1
@@ -138,13 +174,14 @@ def service_partnership():
         sp.append(seq)
     return weight
 
-# Function for simulating new inventions, if new inventions are made it will have a positive impact on the company, if no new invention
-#  is made for two continuous year it will affect the company negatively.
+
+# Function for simulating new inventions, if new inventions are made it will have a positive impact on the company, if
+# no new invention is made for two continuous year it will affect the company negatively.
 def new_invention():
     list_1 = []
     i = 1
     weight = 0
-
+    prob_success = mod_pert_random(0, 75, 100).item()
     while i < 10:
         seq = bool(random.getrandbits(1))
         list_1.append(seq)
@@ -153,7 +190,7 @@ def new_invention():
         if seq:
             list_1.append(False)
             i += 1
-            weight += 90
+            weight += 0.9*prob_success
 
         elif seq is False and list_1[-1] is False:
             weight += -90
@@ -163,13 +200,14 @@ def new_invention():
 
     return weight
 
-# Function for simulating number of countries company is active in, if company is active in more than 45 countires it will have a positive
-# impact on the company, if active country is less than 45 it will affect the company negatively.
+
+# Function for simulating number of countries company is active in, if company is active in more than 45 countires it
+# will have a positive impact on the company, if active country is less than 45 it will affect the company negatively.
 def active_countries(company_name: str):
     weight_ls = []
 
     for i in range(10):
-        if company_name == 'Huawei ':
+        if company_name == 'Apple ':
             act_var = random.randint(50, 60)
         else:
             act_var = random.randint(30, 60)
@@ -252,6 +290,9 @@ if __name__ == '__main__':
     revenue_df = input_file('input/Revenue.csv')
     rndexpenditure_df = input_file('input/RnDExpenditure.csv')
 
+    df_score_columns = ['2018']
+    df_score_yearly = pd.DataFrame(columns=df_score_columns)
+
     for company in company_list:
         print(company)
         company_ms_score = mshare(marketshare_df[marketshare_df['Company'] == company], 5)
@@ -262,7 +303,10 @@ if __name__ == '__main__':
         previous_score_list = [company_ms_score, company_rnd_score, company_profitmargin_score, company_revenue_score]
         company_previous_score = calculate_previous_data_weight(previous_score_list)
 
-        for i in range(5):
+        df_score_yearly.loc[company, '2018'] = company_previous_score
+
+        print(company_previous_score)
+        for i in range(2019, 2024):
             company_score_list = []
             company_country_list = []
             for j in range(0, 1000):
@@ -273,11 +317,15 @@ if __name__ == '__main__':
             company_score_array = np.array(company_score_list)
             company_score_yearly = np.mean(company_score_array)
 
+            df_score_yearly.loc[company, i] = company_score_yearly
+            '''
             company_country_array = np.array(company_country_list)
             company_country_yearly = np.mean(company_country_array)
+            '''
             company_previous_score = company_score_yearly
             print(i)
             print(company)
             print(company_score_yearly)
-            print(company_country_yearly)
+            # print(company_country_yearly)
         print('-'*40)
+    print(df_score_yearly)
